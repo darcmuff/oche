f6a5a4a9cf97c3c4b4923210a52eb6a4292cf541
darcmuff <duff.marc@gmail.com>
2026-09-15 23:03:01 +0000

    Bump cache to oche-v17 for home layout

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* DuffDarts — offline cache; HTML prefers network so updates land */
-var CACHE = 'oche-v16';
+var CACHE = 'oche-v17';
 var SHELL = ['./', './index.html', './sw.js'];
 
 self.addEventListener('install', function (e) {
