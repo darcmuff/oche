e4143c704a2b33fb3c73b8a46273c3eecf3c0ea9
darcmuff <duff.marc@gmail.com>
2026-09-16 13:53:14 +0000

    Bump cache to oche-v21 for big keypad

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* DuffDarts — offline cache; HTML prefers network so updates land */
-var CACHE = 'oche-v20';
+var CACHE = 'oche-v21';
 var SHELL = ['./', './index.html', './sw.js'];
 
 self.addEventListener('install', function (e) {
